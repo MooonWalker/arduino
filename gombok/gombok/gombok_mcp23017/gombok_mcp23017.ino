334dabd1a798362bfc2eb8ae8cafa8d0d878be54
MooonWalker <moonsurveyor@gmail.com>
2024-02-22 10:02:34 +0100

    in

--- gombok/gombok/gombok_mcp23017/gombok_mcp23017.ino
+++ gombok/gombok/gombok_mcp23017/gombok_mcp23017.ino
@@ -2,30 +2,37 @@
 
 #define eport 0x20  // MCP23017 is on I2C port 0x20 B0100000 7 bit 0100A1A2A0
 #define INTCAPA 0x08
+#define INTCAPB 0x18
 
 byte inputs=0;             // Variable to store the Byte in
 volatile boolean keypressed;
 unsigned long time = 0;
 
+const int buttonPin = 2;  // the number of the pushbutton pin
+
 void setup()
 {
   Serial.begin(9600); 
   Wire.begin();                // wake up I2C bus
-  
-  expanderWrite(0x0B, B10000000); //IOCONZ, BANK1  
+  pinMode(buttonPin,    INPUT_PULLUP);
+  expanderWrite(0x0B, B10000000); //IOCON, BANK1  
                                   // valid command only if BANK = 0, which is true upon reset
                                   // set BANK = 1 if it had been 0 (nothing happens if BANK = 1 already)
-  expanderWrite(0x06, 0xff); //GPPUA                               
-  expanderWrite(0x01, 0xff); //IPOLA                               
-  expanderWrite(0x02, 0xff); //GPINTENA
+  //expanderWrite(0x06, 0xff); //GPPUA                               
+  expanderWrite(0x16, 0x7f); //GPPUB
+  //expanderWrite(0x01, 0xff); //IPOLA                               
+  expanderWrite(0x11, 0xff); //IPOLB
+  //expanderWrite(0x02, 0xff); //GPINTENA
+  expanderWrite(0x12, 0xff); //GPINTENB
   
   keypressed=false;
   
   // read from interrupt capture ports to clear them
-  expanderRead(INTCAPA); //INTCAPA   
+  expanderRead(INTCAPB); //INTCAPA   
     
-  attachInterrupt(0, keypress, FALLING);
-
+  //attachInterrupt(0, keypress, FALLING);
+  attachInterrupt(digitalPinToInterrupt(buttonPin), keypress, FALLING);
+    Serial.println("Configured");
 }
 
 void loop()
@@ -37,11 +44,11 @@ void loop()
 //  inputs=Wire.read();  
   
    
-  if(keypressed)
-  {
-    handleKeypress();    
-    
-  }
+    if(keypressed)
+    {
+        handleKeypress();    
+        
+    }
   
   // turn LED off after 500 ms 
  if (millis() > (time + 500) && time != 0)
@@ -51,7 +58,7 @@ void loop()
       time = 0;
    }  // end if time up
    
-}   //Endloop
+}   //-----------------------Endloop
 
 void keypress()
 {
@@ -84,7 +91,7 @@ void handleKeypress()
   delay(100); // de-bounce before we re-enable interrupts
   keypressed = false;
   
-  keyValue= expanderRead(INTCAPA);
+  keyValue= expanderRead(INTCAPB);
   if(keyValue==192) keyValue= 5;
   if(keyValue==8) keyValue= 4;
   if(keyValue==32) keyValue= 3;
